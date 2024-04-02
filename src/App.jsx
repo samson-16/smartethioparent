4d323dd7d8e3f3317046f68d63e7de678caa725e
Ayub Sufian <ayubsufiane@gmail.com>
2024-04-02 17:10:24 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-
 import './App.css'
+<<<<<<< Updated upstream
 import LandingPage from './pages/LandingPage'
+import './App.css'
+import Footer from './Footer'
+>>>>>>> Stashed changes
 
 function App() {
 
 
   return (
+<<<<<<< Updated upstream
     
    <LandingPage />
   
+=======
+<div>
+    <Footer/>
+  </div>
+>>>>>>> Stashed changes
   )
 }
 
