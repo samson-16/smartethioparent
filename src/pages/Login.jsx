ef514080c84fa9c3c8a4fd785ea5652f485c22ce
Ayub Sufian <ayubsufiane@gmail.com>
2024-04-14 02:13:06 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useContext} from 'react';
+import { useState , useContext} from 'react';
 import { AuthContext } from '../components/AuthContext.jsx';
 function Login() {
     const [email, setEmail] = useState('');
