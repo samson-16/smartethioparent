14c5d75b1ee7edd7536b4a7fddac3f8d43921ab9
Ayub Sufian <ayubsufiane@gmail.com>
2024-04-14 01:58:22 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -67,23 +67,27 @@ function Login() {
     };
 
     return (
-        <section className='flex justify-center items-center mt-28'>
-            <div className='bg-blue-600 w-[400px] h-64 flex flex-col justify-center items-center rounded-lg'>
-            <h2 className="text-white text-2xl mb-4">Login</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col space-y-3">
-                <div className='mb-3'>
-                    {/* <label htmlFor="email" className="text-white">Email:</label> */}
-                    <input id="email" type="email" value={email} onChange={handleEmailChange} placeholder="Enter your email" className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400" />
+<section className='flex mt-20 justify-between flex-wrap'>
+            <div className='flex flex-col rounded-lg ml-24 w-80 h-64'>
+                <h2 className="text-black text-2xl font-bold mb-9 pl-0">Login</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col space-y-3">
+                    <div className='mb-4'>
+                    <label htmlFor="email" className="block font-semibold">Email Address</label>
+                    <input id="email" type="email" value={email} onChange={handleEmailChange} placeholder="Enter your email" className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400 mt-4 w-80" />
                 </div>
-                <div className="mb-3">
-                    {/* <label htmlFor="password" className="text-white">Password:</label> */}
-                    <input id="password" type="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400" />
-                </div>
-                <div className='flex justify-center'>
-                <button type="submit" className="bg-white text-blue-400 py-2 px-4 rounded-md hover:bg-blue-400 hover:text-white transition duration-300 w-40 flex justify-center font-bold">Login</button>
-                </div>
-            </form>
-        </div>
+               <div className="mb-3 flex justify-between items-center">
+                <label htmlFor="password" className="block font-semibold">Password</label>
+                <a href="#" className="text-blue-500 hover:text-blue-700 underline text-sm">Forgot Password?</a>
+            </div>
+            <div>
+                <input id="password" type="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-400 mt-1 w-80 mb-8" />
+            </div>
+                    <div className='flex'>
+                        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300 w-80 flex justify-center font-bold">LOGIN</button>
+                    </div>
+                </form>
+            </div>
+            <img src="/src/assets/kidStudying.png" alt="Kid studying with parent" className="w-[530px] h-[420px] mr-28"/>
         </section>
     );
 }
